4ff5638241ab0a3f4d2970a0acfc815de1796b4b
sandra207 <sandrasaju207@gmail.com>
2026-06-28 13:35:28 +0530

    add tracking component

--- Frontend/src/components/Table.jsx
+++ Frontend/src/components/Table.jsx
@@ -424,7 +424,7 @@ const updateTracking = async (
   </span>
 
  <a
-  href={`https://www.dtdc.in/tracking/tracking_results.asp?Ttype=awb_no&strCnno=${order.trackingId}`}
+  href={`https://www.dtdc.in/tracking/tracking_results.asp?Ttype=awb_no&strCnno=${item.trackingId}`}
   target="_blank"
   rel="noopener noreferrer"
   className="inline-block mt-3 px-4 py-2 bg-amber-500 text-black rounded-lg font-semibold hover:bg-amber-400"
